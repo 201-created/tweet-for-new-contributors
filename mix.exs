c4d68bfab1299605965e53f1f25ed08564bdf81d
Kevin Pfefferle <kevin@pfefferle.co>
2018-11-09 13:58:58 -0500

    Update deps per Phoenix 1.4 upgrade guide

--- mix.exs
+++ mix.exs
@@ -40,8 +40,8 @@ defmodule Twitterbot.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 2.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:plug, "~> 1.7"},
       {:ecto_sql, "~> 3.0"},
 
       {:httpotion, "~> 3.1.0"},
